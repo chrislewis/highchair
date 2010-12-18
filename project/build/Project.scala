58962f98106c149c25de2939a762967a628c3e6e
chrislewis <chris@thegodcode.net>
2010-12-18 13:35:22 -0500

    proper scope

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -3,10 +3,10 @@ import sbt._
 class Project(info: ProjectInfo) extends DefaultProject(info) {
   
   val gae_version = "1.3.7"
-  val gaeSdk = "com.google.appengine" % "appengine-api-1.0-sdk" % gae_version % "compile"
-  val gaeStubs = "com.google.appengine" % "appengine-api-stubs" % gae_version % "compile"
-  val gaeTest = "com.google.appengine" % "appengine-testing" % gae_version % "compile"
-  val gaeApiLabs = "com.google.appengine" % "appengine-api-labs" % gae_version % "compile"
+  val gaeSdk = "com.google.appengine" % "appengine-api-1.0-sdk" % gae_version % "provided"
+  val gaeStubs = "com.google.appengine" % "appengine-api-stubs" % gae_version % "provided"
+  val gaeTest = "com.google.appengine" % "appengine-testing" % gae_version % "provided"
+  val gaeApiLabs = "com.google.appengine" % "appengine-api-labs" % gae_version % "provided"
   
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test"
   
