df965ae65ffc569da9ca144fa8a232b6e1e1c3ac
chrislewis <chris@thegodcode.net>
2010-12-22 23:01:10 -0500

    cell queries now ? and ??; geocell specs

--- geocell/src/main/scala/GeoCell.scala
+++ geocell/src/main/scala/GeoCell.scala
@@ -13,6 +13,7 @@ object GeoCell {
   val MAX_RESOLUTION = 13
   val GRID_SIZE = 4
   val ALPHABET = "0123456789abcdef"
+  val RADIUS = 6378135 /* meters */
   
   // Returns the geocell character in the 4x4 alphabet grid at pos. (x, y).
   def subdivChar(pos: (Int, Int)) =
@@ -23,14 +24,14 @@ object GeoCell {
       (pos._1 & 1) << 0)
   
   /** Calculate the geocell for a point at the given resolution. */
-  def compute(point: Point, resolution: Int = MAX_RESOLUTION): String = {
+  def ?(point: Point, resolution: Int = MAX_RESOLUTION): String = {
     def _compute(n: Double, s: Double, e: Double, w: Double, cell: String = "", depth: Int = 0): String = {
       if(depth >= resolution)
         cell
       else {
-        val x = Math.min((GRID_SIZE * (point._2 - w) / (e - w)),
+        val x = math.min((GRID_SIZE * (point._2 - w) / (e - w)),
           GRID_SIZE - 1).toInt;
-        val y = Math.min((GRID_SIZE * (point._1 - s) / (n - s)),
+        val y = math.min((GRID_SIZE * (point._1 - s) / (n - s)),
           GRID_SIZE - 1).toInt;
         val subcellLonSpan = (e - w) / GRID_SIZE
         val subcellLatSpan = (n - s) / GRID_SIZE
@@ -47,4 +48,27 @@ object GeoCell {
     }
     _compute(90d, -90d, 180d, -180d)
   }
+  
+  /** Calculate the geocells in which the point resides at every resolution. */
+  def ??(point: Point) = 
+    (1 to MAX_RESOLUTION) map(?(point, _)) toList
+  
+  /** Calculate the distance between two points in meters. */
+	def distance(p1: Point, p2: Point) = {
+		val p1lat = math.toRadians(p1._1)
+		val p1lon = math.toRadians(p1._2)
+		val p2lat = math.toRadians(p2._1)
+		val p2lon = math.toRadians(p2._2)
+		RADIUS * math.acos(fixDouble(math.sin(p1lat) * math.sin(p2lat)
+						+ math.cos(p1lat) * math.cos(p2lat)
+						* math.cos(p2lon - p1lon)));
+	}
+	
+	def fixDouble(dbl: Double): Double = 
+	  if (dbl > 1)
+	    1
+	  else if (dbl < -1)
+	    -1
+	  else
+	    dbl
 }
---
+++ geocell/src/test/scala/GeoCellSpec.scala
@@ -0,0 +1,46 @@
+package highchair.geocell
+
+import org.specs._
+
+class GeoCellSpec extends Specification {
+  
+  val st_augustine = (30.332184, -81.655651)
+  val jacksonville = (29.896466, -81.313248)
+  val firenze =      (37.0625,   -95.677068)
+  
+  "Saint Augustine" should {
+    "be in cell 989c8471bbd5d at max resolution" in {
+      GeoCell ? st_augustine must_== "989b5c5f82e47"
+    }
+    
+    "be in cell 9 at resolution 1" in {
+      GeoCell ? (st_augustine, 1) must_== "9"
+    }
+    
+    "be about 58649 meters from Jacksonville" in {
+      GeoCell distance (st_augustine, jacksonville) must beCloseTo(58649d, 5d) /* Give or take 5 meters. */
+    }
+  }
+  
+  "Jacksonville" should {
+    "be in cell 989c8471bbd5d at max resolution" in {
+      GeoCell ? jacksonville must_== "989c8471bbd5d"
+    }
+    
+    "be in cell 9 at resolution 1" in {
+      GeoCell ? (jacksonville, 1) must_== "9"
+    }
+  }
+  
+  "Firenze" should {
+    "essere nella cella 8f65df3c409b3 alla massima risoluzione" in {
+      GeoCell ? firenze must_== "8f65df3c409b3"
+    }
+    
+    "essere nella cella 8 alla risoluzione 1" in {
+      GeoCell ? (firenze, 1) must_== "8"
+    }
+  }
+  
+}
+
