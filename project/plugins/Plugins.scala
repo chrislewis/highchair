2a3efc569f4275224e033df8c86c22eb4394913c
chrislewis <chris@thegodcode.net>
2011-01-03 15:51:44 -0500

    net.thegodcode; remove appengine plugin dependency (artifiacts provide the environment)

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,6 +1,5 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val gae = "net.stbbs.yasushi" % "sbt-appengine-plugin" % "2.1"
   val posterous = "net.databinder" % "posterous-sbt" % "0.1.6"
 }
