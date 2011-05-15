6542e75227e971cca6d3af099e8c0031557210b2
chrislewis <chris@thegodcode.net>
2011-05-15 16:02:52 -0400

    is/not filters alias ===/!==, respectively

--- datastore/src/main/scala/filters.scala
+++ datastore/src/main/scala/filters.scala
@@ -25,7 +25,9 @@ sealed class PropertyFilter[E, A](val property: PropertyMapping[E, A]) {
   
   /* Filter operations. */
   def === (value: A)  = single(FO.EQUAL, value)
+  def is (value: A)   = ===(value)
   def !== (value: A)  = single(FO.NOT_EQUAL, value)
+  def not (value: A)  = !==(value)
   def >   (value: A)  = single(FO.GREATER_THAN, value)
   def >=  (value: A)  = single(FO.GREATER_THAN_OR_EQUAL, value)
   def <   (value: A)  = single(FO.LESS_THAN, value)
