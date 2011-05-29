0ec9ebb3eaded6cd5035ed54dd033b5d9e86a702
chrislewis <chris@thegodcode.net>
2011-05-29 15:09:06 -0400

    order* takes  K => PropertyMapping to enable syntax consistent w/ filters

--- datastore/src/main/scala/Query.scala
+++ datastore/src/main/scala/Query.scala
@@ -4,7 +4,6 @@ import meta._
 
 import com.google.appengine.api.datastore.{
   DatastoreService,
-  Entity => GEntity,
   FetchOptions,
   Query => GQuery
 }
@@ -14,21 +13,20 @@ case class Query[E <: Entity[E], K <: Kind[E]](
   filters:  List[Filter[E, _]],
   sorts:    List[Sort[E, _]]) {
   
-  def and(f: K => Query[E, K]) = f(kind) match {
-    case Query(_, fs, ss) =>
-      Query(kind, fs ::: filters, ss ::: sorts)
-  }
+  def and(f: K => Filter[E, _]) =
+    Query(kind, f(kind) :: filters, sorts)
   
-  def orderAsc[A](p: PropertyMapping[E, A]) =
-    Query(kind, filters, FilterOps.Asc(p) :: sorts)
+  def orderAsc(f: K => PropertyMapping[E, _]) =
+    Query(kind, filters, Asc(f(kind)) :: sorts)
   
-  def orderDesc[A](p: PropertyMapping[E, A]) =
-    Query(kind, filters, FilterOps.Desc(p) :: sorts)
+  def orderDesc(f: K => PropertyMapping[E, _]) =
+    Query(kind, filters, Desc(f(kind)) :: sorts)
   
   def fetchOne()(implicit dss: DatastoreService) = 
     fetch() headOption
   
-  def fetch(offset: Int = 0, limit: Int = 500)(implicit dss: DatastoreService) = { //TODO better default
+  // TODO better default + clean up
+  def fetch(offset: Int = 0, limit: Int = 500)(implicit dss: DatastoreService) = {
     val q = bindParams(new GQuery(kind.reflector.simpleName), (filters ::: sorts):_*)
     val opts = FetchOptions.Builder withOffset(offset) limit(limit)
     collection.JavaConversions.asIterable(dss.prepare(q).asIterable(opts)) map kind.entity2Object
@@ -36,9 +34,4 @@ case class Query[E <: Entity[E], K <: Kind[E]](
   
   private def bindParams(q: GQuery, params: Filter[E, _]*) =
     (q /: params) { (q, f) => f bind q }
-}
-
-object Query { //TODO saner implicitry
-  implicit def Filter2Query[E <: Entity[E], K <: Kind[E]](f: Filter[E, _])(implicit kind: K) =
-    Query(kind, List(f), Nil)
 }
