dfbe6feb39da2818c3fe3f74f96bc1975dc6ccdd
chrislewis <chris@thegodcode.net>
2011-05-29 15:11:45 -0400

    PropertyFilters must be PropertyMapping; PropertyMapping refs its Kind (used by Query), escalated type constraints E <: highchair.datastore.Entity[E]; FilterOps dropped

--- datastore/src/main/scala/filters.scala
+++ datastore/src/main/scala/filters.scala
@@ -12,11 +12,11 @@ sealed trait Filter[E, A] {
   def bind(q: GQuery): GQuery
 }
 
-sealed class PropertyFilter[E, A](val property: PropertyMapping[E, A]) {
+private[meta] trait PropertyFilter[E <: highchair.datastore.Entity[E], A] { this: PropertyMapping[E, A] =>
   
   def single(filter: FO, value: A) = new Filter[E, A] {
     def bind(q: GQuery) = {
-      q.addFilter(property.name, filter, property.prop.toStoredType(value))
+      q.addFilter(name, filter, prop.toStoredType(value))
     }
   }
   
@@ -24,7 +24,7 @@ sealed class PropertyFilter[E, A](val property: PropertyMapping[E, A]) {
     def bind(q: GQuery) = {
       val list = new java.util.ArrayList[Any]
       value foreach list.add
-      q.addFilter(property.name, filter, list)
+      q.addFilter(name, filter, list)
     }
   }
   
@@ -40,23 +40,17 @@ sealed class PropertyFilter[E, A](val property: PropertyMapping[E, A]) {
   def in  (value: A*) = multi(FO.IN, value:_*)
 }
 
-@deprecated("use datastore.Query", "0.0.4")
-case class Query[E](val filters: List[Filter[E, _]], val sorts: List[Sort[E, _]]) {
-  def &&(f: Filter[E, _]) = Query(f :: filters, sorts)
-  def sort(s: Sort[E, _]) = Query(filters, s :: sorts)
-}
-
 /* Special case filter to add sorts while taking advantage of a common interface. */
-sealed abstract class Sort[E, A](val p: PropertyMapping[E, A], val direction: SD)
+sealed abstract class Sort[E <: highchair.datastore.Entity[E], A](val p: PropertyMapping[E, A], val direction: SD)
   extends Filter[E, A] {
   def bind(q: GQuery) = q.addSort(p.name, direction)
 }
 
-object FilterOps {
-  /* Sorts. Eventually these will have a more logical home, but for now don't require additional imports. */
-  case class Asc[E, A](val property: PropertyMapping[E, A]) extends Sort(property, SD.ASCENDING)
-  case class Desc[E, A](val property: PropertyMapping[E, A]) extends Sort(property, SD.DESCENDING)
-  
-  implicit def Property2Filter[E, A](pm: PropertyMapping[E, A]) = new PropertyFilter(pm)
-  implicit def Filter2Query[E](f: Filter[E, _]) = Query(List(f), Nil)
+case class Asc[E <: highchair.datastore.Entity[E], A](val property: PropertyMapping[E, A]) extends Sort(property, SD.ASCENDING)
+case class Desc[E <: highchair.datastore.Entity[E], A](val property: PropertyMapping[E, A]) extends Sort(property, SD.DESCENDING)
+
+@deprecated("use datastore.Query", "0.0.4")
+case class Query[E <: highchair.datastore.Entity[E]](val filters: List[Filter[E, _]], val sorts: List[Sort[E, _]]) {
+  def &&(f: Filter[E, _]) = Query(f :: filters, sorts)
+  def sort(s: Sort[E, _]) = Query(filters, s :: sorts)
 }
