cc991ea9f0b2cb63ea6b8ca5a51d2bb171669ad9
chrislewis <chris@thegodcode.net>
2011-05-29 18:41:23 -0400

    tidying Query

--- datastore/src/main/scala/Query.scala
+++ datastore/src/main/scala/Query.scala
@@ -14,32 +14,34 @@ case class Query[E <: Entity[E], K <: Kind[E]](
   filters:  List[Filter[E, _]],
   sorts:    List[Sort[E, _]]) {
   
-  def rawQuery =
-    bindParams(new GQuery(kind.reflector.simpleName), (filters ::: sorts):_*)
-  
+  /** Construct a native datastore query. */
+  def rawQuery = 
+    (baseQuery /: (filters ::: sorts)) { (q, f) => f bind q }
+  /** Add a filter. */
   def and(f: K => Filter[E, _]) =
     copy(filters = f(kind) :: filters)
-  
+  /** Sort ascending on some property. TODO moar types */
   def orderAsc(f: K => PropertyMapping[E, _]) =
     copy(sorts = Asc(f(kind)) :: sorts)
-  
+  /** Sort descending on some property. TODO moar types */
   def orderDesc(f: K => PropertyMapping[E, _]) =
     copy(sorts = Desc(f(kind)) :: sorts)
   
+  private def baseQuery = new GQuery(kind.reflector.simpleName)
+
+  /** Fetch a single record matching this query. */
   def fetchOne()(implicit dss: DatastoreService) = 
     fetch() headOption
-  
+  /** Fetch only the keys of entities matching this query. More efficient. */
   def fetchKeys()(implicit dss: DatastoreService) = {
     val q = rawQuery setKeysOnly()
     collection.JavaConversions.asIterable(dss.prepare(q).asIterable) map (_ getKey)
   }
-    
-  // TODO better default + clean up
+  /** Fetch entities matching this query, optionally providing limits and/or offsets. */
+  // TODO better default; clean up; what happens when offset extends the bounds?
   def fetch(limit: Int = 500, offset: Int = 0)(implicit dss: DatastoreService) = {
     val opts = FetchOptions.Builder withOffset(offset) limit(limit)
     collection.JavaConversions.asIterable(dss.prepare(rawQuery).asIterable(opts)) map kind.entity2Object
   }
   
-  private def bindParams(q: GQuery, params: Filter[E, _]*) =
-    (q /: params) { (q, f) => f bind q }
 }
